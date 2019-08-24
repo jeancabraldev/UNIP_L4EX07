217cfb25f1d029509f4678636f8076959bdfb275
jeancabraldev <54410614+jeancabraldev@users.noreply.github.com>
2019-08-24 16:30:58 -0300

    Add files via upload

---
+++ L4EX07_jogo_soma/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));;
+
+    int numUsuario;
+    int total;
+    char resp = 's';
+
+    while(resp == 's'){
+        if(resp == 's'){
+            int num1 = rand() % 999;
+            int num2 = rand() % 999;
+            total = num1 + num2;
+            printf("Qual a soma de %d + %d?\n", num1, num2);
+            scanf("%d", &numUsuario);
+
+            if(numUsuario == total){
+                printf("\nParabens voce acertou!\n");
+                printf("\nResultado: %d", total);
+        } else {
+            printf("\nQue pena voce errou!\n");
+            printf("\nResultado: %d", total);
+          }
+          printf("\n\nQuer continuar jogando?\n");
+          scanf(" %c", &resp);
+        }
+    }
+    return 0;
+}
